c4eee4fae61f4df4e7f3e70909cd890457767925
Aaron Demski <ajdemski@frontier.com>
2023-07-14 11:28:14 -0700

    added Welcome message with item quantity

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace awpp
+namespace app
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread: $5 each or buy 2, get 1 free");
+            Console.WriteLine("Pastry: $2 each or buy 3, get 1 free");
+
+            Console.WriteLine("Enter the number of Bread you'd like to purchase: ");
+            int Bread = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of Pastries you'd like to purchase: ");
+            int Pastry = int.Parse(Console.ReadLine());
         }
     }
 }
