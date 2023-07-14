72cd5462cbdee6ee92f473780487a0482a3f7acb
Aaron Demski <ajdemski@frontier.com>
2023-07-14 15:50:35 -0700

    trying to add tests to my project since I have time and forgot to do

---
+++ Tests/ModelTests/AppTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using app.Models;
+
+namespace app.Tests
+{
+  [TestClass]
+  public class AppTests
+  {
+    [TestMethod]
+    public void TestMethod()
+    {
+      // test logic 
+    }
+    [TestMethod]
+    public void TestMethod1()
+    {
+      // test logic 
+    }
+
+  }
+}
