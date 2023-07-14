512c234beda2812d2db447ccf14bce89c973a5b7
Aaron Demski <ajdemski@frontier.com>
2023-07-14 10:53:11 -0700

    had pc problems starting up project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace awpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
